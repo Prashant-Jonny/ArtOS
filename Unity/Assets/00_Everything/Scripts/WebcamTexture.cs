bb7c76e57639073d7ddf360e238b9adcb7ffdce1
edwonia <edwonia@gmail.com>
2015-02-09 02:24:12 +0900

    webcam texture working sort of but crashy

---
+++ Unity/Assets/00_Everything/Scripts/WebcamTexture.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamTexture : MonoBehaviour {
+
+	void Start () 
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( var i = 0 ; i < devices.Length; i++ )
+		{
+			Debug.Log(devices[i].name);
+		}
+		WebCamTexture webcam = new WebCamTexture();
+		webcam.deviceName = "FaceTime HD Camera (Built-in)";
+		gameObject.GetComponent<Renderer>().material.mainTexture = webcam;
+		webcam.Play();
+
+//		WebCamTexture webcamTexture = new WebCamTexture();
+//		this.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+//		webcamTexture.Play();
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
