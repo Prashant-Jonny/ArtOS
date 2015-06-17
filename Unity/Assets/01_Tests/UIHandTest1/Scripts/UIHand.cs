8c6ce66259207e192e9e17be0d975358b812a7a2
Edwon <edwonia@gmail.com>
2015-06-16 18:30:02 -0700

    UIHandTest getting hands from leap controller

--- Unity/Assets/01_Tests/UIHandTest1/Scripts/UIHand.cs
+++ Unity/Assets/01_Tests/UIHandTest1/Scripts/UIHand.cs
@@ -4,18 +4,31 @@ using Leap;
 
 namespace UIHandTest1 
 {
-	public class UIHand : MonoBehaviour 
-	{
-
-		void Start () 
-		{
-			
-		}
-		
-		void Update ()
-		{
-			
-		}
-
+public class UIHand : MonoBehaviour 
+{
+	public GameObject leapMotionOVRController = null;
+	public HandController handController = null;
+	
+	// Use this for initialization
+	void Start () {
 	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (leapMotionOVRController == null || handController == null)
+			return;
+		
+		HandModel[] hands = handController.GetAllGraphicsHands();
+		if (hands.Length > 1)
+		{
+			Vector3 direction0 = (hands[0].GetPalmPosition() - handController.transform.position).normalized;
+			Vector3 normal0 = hands[0].GetPalmNormal().normalized;
+			
+			Vector3 direction1 = (hands[1].GetPalmPosition() - handController.transform.position).normalized;
+			Vector3 normal1 = hands[1].GetPalmNormal().normalized;
+
+		}
+	}
+
+}
 }
