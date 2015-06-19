6b86800028f78ac46225dde7d7efe5bccd16f57a
Edwon <edwonia@gmail.com>
2015-06-18 17:23:13 -0700

    deleting old commented out stuff from leaputil

--- Unity/Assets/01_Tests/UIHandTest1/Scripts/LeapUtil.cs
+++ Unity/Assets/01_Tests/UIHandTest1/Scripts/LeapUtil.cs
@@ -59,36 +59,6 @@ using Leap;
 		{
 			return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
 		}
-
-//		public static Vector3 ToPositionVector3 (Vector position)
-//		{
-//			return new Vector3(position.x, position.y, -position.z);
-//		}
-//
-//		public static Vector3 ToVector3(Vector v)
-//		{
-//			return new Vector3(v.x,v.y,v.z);
-//		}
-
-//		public static void LookAt (Transform t, Vector normal)
-//		{
-//			t.LookAt(t.position + ToPositionVector3(normal), Vector3.forward);
-//		}
-//
-//
-//		public static Vector3 LeapToWorld(Vector leapPoint, InteractionBox iBox)
-//		{
-//			leapPoint.z *= -1.0f; // right ahnd to left hand rule
-//			Vector normalized = iBox.NormalizePoint(leapPoint, false);
-//			normalized -= new Vector(0.5f, -.5f, 0.5f);//recenter origin
-//		
-//			Vector n = normalized * 5f; //scale
-//			return new Vector3 (n.x,n.y,n.z);
-//		}
-//
-//		public static Vector3 HandToDirection(Hand hand)
-//		{
-//			return new Vector3(hand.Direction.x, hand.Direction.y, -hand.Direction.z);
-//		}
+	
 	}
 //}
